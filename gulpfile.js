a5e0586e5d7ebd477aa12cc0a14be234bc1266db
Alberto Leal <mail4alberto@gmail.com>
2014-01-05 08:03:07 -0500

    testbed set up for coffee config options
    regarding https://github.com/wearefractal/gulp-coffee/issues/2

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var EE = require('events').EventEmitter;
 
 var es = require('event-stream');
 
-
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
@@ -14,75 +13,29 @@ var path = require('path');
 var srcCoffeeDir = './coffee/';
 var destDir = './src/';
 
-// Emulate coffee -b -w -c -o ./src/ ./coffee
-var gulpCoffee = function(target) {
-
-var _gulpCoffee = coffee({bare: true});
-
-
-
-// clean stream of onerror
-var cleaner = function(stream) {
-    stream.listeners('error').forEach(function(item) {
-        if(item.name == 'onerror') this.removeListener('error', item);
-            // console.log('removed listener ('+ item.name +') for error');
-    }, stream);
-};
-
-
-var continueOnErrorPipe  = function() {
-
-    // backwards compatible passthrough??
-    return es.through(function(data) {
-        this.emit('data', data);
-    })
-    .on('error', gutil.log)
-    .on('pipe', function(src) {
-        cleaner(src);
-    })
-
-    .on('newListener', function() {
-        cleaner(this);
-    });
-
-}
-
-// decorator version
 var continueOnError = function(stream) {
+
     return stream
     .on('error', function() {})
-    .on('pipe', function(src) {
-        cleaner(src);
-    })
-
     .on('newListener', function() {
-        cleaner(this);
+       this.listeners('error').forEach(function(f) {
+           if(f.name == 'onerror') this.removeListener('error', f);
+       }, this);
     });
 };
 
-        gulp.src(target)
+var gulpCoffee = function(target) {
+
+    var coffeeStream = coffee({ bare: true });
+
+    gulp.src(target)
         .on('data', function(file){
             file['original_file_path'] = file.path;
         })
-
-        .pipe(continueOnError(_gulpCoffee))
-
+        .pipe(continueOnError(coffeeStream))
             .on('error', gutil.log)
-            // .on('error', gutil.beep)
-
-            // Another way to hook on error
-            // _gulpCoffee.on('error', function dashed(err) {
-
-            //     gutil.log(err);
-            //     console.log('error listener count:' + EE.listenerCount(this, 'error'));
-
-            //     // let's see current error listener
-            //     console.log(this.listeners('error'));
-            // })
-
-
         .pipe(gulp.dest(destDir))
-            .on('data', function(file) {
+           .on('data', function(file) {
 
 
 
@@ -97,8 +50,8 @@ var continueOnError = function(stream) {
                 gutil.log("Compiled '" + from + "' to '" + to + "'");
 
             });
-
 };
+
 
 // The default task (called when you run `gulp`)
 gulp.task('default', function() {
@@ -107,6 +60,7 @@ gulp.task('default', function() {
 
     // Process all coffee files.
     gulpCoffee(target);
+    // gulpCoffee()
 
     // Watch coffeescript files and compile them if they change
     // gulp.watch(target, function(event) {
--- src/subdir/a.js
+++ src/subdir/a.js
@@ -1,1 +1,48 @@
-a = 1;
+Animal = (function() {
+  function Animal(name) {
+    this.name = name;
+  }
+
+  Animal.prototype.move = function(meters) {
+    return alert(this.name + (" moved " + meters + "m."));
+  };
+
+  return Animal;
+
+})();
+Snake = (function(_super) {
+  __extends(Snake, _super);
+
+  function Snake() {
+    _ref = Snake.__super__.constructor.apply(this, arguments);
+    return _ref;
+  }
+
+  Snake.prototype.move = function() {
+    alert("Slithering...");
+    return Snake.__super__.move.call(this, 5);
+  };
+
+  return Snake;
+
+})(Animal);
+Horse = (function(_super) {
+  __extends(Horse, _super);
+
+  function Horse() {
+    _ref1 = Horse.__super__.constructor.apply(this, arguments);
+    return _ref1;
+  }
+
+  Horse.prototype.move = function() {
+    alert("Galloping...");
+    return Horse.__super__.move.call(this, 45);
+  };
+
+  return Horse;
+
+})(Animal);
+sam = new Snake("Sammy the Python");
+tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move();
